14ed914f030b9883317a23ec352b02dd832ec55b
github-action-benchmark <github@users.noreply.github.com>
2022-12-03 16:31:43 +0000

    add My Project Go Benchmark (go) benchmark result for 3bec555844bb1265a30d9ab6148c51592fc31235

---
+++ dev/bench/data.js
@@ -0,0 +1,68 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1670085103922,
+  "repoUrl": "https://github.com/BradErz/aoc",
+  "entries": {
+    "My Project Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "bwilsonhunt@gmail.com",
+            "name": "Bradley Wilson-Hunt",
+            "username": "BradErz"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3bec555844bb1265a30d9ab6148c51592fc31235",
+          "message": "ci: adding benchmarks (#3)\n\n* ci: adding benchmarks\r\n\r\n* chore: adding benchmarks",
+          "timestamp": "2022-12-03T16:29:10Z",
+          "tree_id": "3b6c76bc38b13338841bac4b6f6443a55883da54",
+          "url": "https://github.com/BradErz/aoc/commit/3bec555844bb1265a30d9ab6148c51592fc31235"
+        },
+        "date": 1670085103523,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkDay01Part1",
+            "value": 92885,
+            "unit": "ns/op\t   16880 B/op\t    2014 allocs/op",
+            "extra": "12878 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDay01Part2",
+            "value": 96274,
+            "unit": "ns/op\t   16880 B/op\t    2014 allocs/op",
+            "extra": "12547 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDay02Part1",
+            "value": 468761,
+            "unit": "ns/op\t   92112 B/op\t    5002 allocs/op",
+            "extra": "2460 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDay02Part2",
+            "value": 459660,
+            "unit": "ns/op\t   92112 B/op\t    5002 allocs/op",
+            "extra": "2532 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDay03Part1",
+            "value": 78251,
+            "unit": "ns/op\t   10632 B/op\t     138 allocs/op",
+            "extra": "15364 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkDay03Part2",
+            "value": 319385,
+            "unit": "ns/op\t   64767 B/op\t     646 allocs/op",
+            "extra": "3554 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
